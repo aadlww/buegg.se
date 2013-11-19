c816b14322ef9d2da25fe9a0c0c3b1e086e8a23f
Anders Åslund <anders.aslund@me.com>
2013-11-19 19:27:07 +0100

    no message

--- model.js
+++ model.js
@@ -8,6 +8,78 @@ facebookPostsLimit = 15; //TODO: check if in database and if so set it
 loadingMsg = 'Laddar...'; //TODO: check if in database and if so set it
 loadingIcon = '/images/loader2.gif'; //TODO: check if in database and if so set it
 
+function fbUpdate()
+{
+	console.log('Updating from facebook...');
+	FBPhotos.remove({});
+	FBGRAPH.get(facebookURL+"/albums?fields=name,photos.limit(" + facebookPostsLimit + ").fields(images,link,name,created_time)", Meteor.bindEnvironment( function(err, res) {
+		res.data.forEach(function(album) {
+			if (album.name == "Timeline Photos"){
+		 		album.photos.data.forEach(function(photo){
+		 			// console.log(photo)
+		 			FBPhotos.insert(photo);
+		 		});
+		 	}
+		 	if (album.name == "Mobile Uploads"){
+		 		album.photos.data.forEach(function(photo){
+		 			// console.log(photo)
+		 			FBPhotos.insert(photo);
+		 		});
+		 	}
+		 });
+	}, function(e) {
+		Meteor._debug("Exception from get facebook logo:", e);
+	}));
+
+	//console.log('getPageFBLogo'); 
+	//FBGRAPH.setAccessToken(Meteor.user().services.facebook.accessToken);
+	FBGRAPH.get(facebookURL+"?fields=picture.width(130)", Meteor.bindEnvironment( function(err, res) {
+		if (res.picture) {
+			//console.log(res.picture.data);
+			FBSiteInfo.update(site[0]._id, {'$set' : {logo : res.picture.data.url}});
+		}
+	}, function(e) {
+		Meteor._debug("Exception from get facebook logo:", e);
+	}));
+
+	//console.log('getPageFBData'); 
+	FBGRAPH.get(facebookURL+"?fields=about,name,phone", Meteor.bindEnvironment( function(err, res) {
+		//console.log(res);
+		if (res.about) {
+			FBSiteInfo.update(site[0]._id, {'$set' : {about : res.about}});
+		}
+		if (res.name) {
+			FBSiteInfo.update(site[0]._id, {'$set' : {name : res.name}});
+		}
+		if (res.phone) {
+			FBSiteInfo.update(site[0]._id, {'$set' : {phone : res.phone}});
+		}
+	}, function(e) {
+		Meteor._debug("Exception from get facebook data:", e);
+	}));
+
+	FBGRAPH.get(facebookURL+"?posts.limit(1)", Meteor.bindEnvironment( function(err, res) {
+		// console.log(res);
+		if (res.posts) {
+			FBSiteInfo.update(site[0]._id, {'$set' : {posts : res.posts.data[0].message}});
+		}
+	}, function(e) {
+    		Meteor._debug("Exception from get facebook posts:", e);
+	}));
+
+	FBGRAPH.get(facebookURLOwner+"?fields=email,name,about,picture.width(130)", Meteor.bindEnvironment( function(err, res) {
+		// console.log(res);
+		if (res.name){
+			FBSiteInfo.update(site[0]._id, {'$set' : {owner_name : res.name}});
+		}
+		if (res.picture) {
+			FBSiteInfo.update(site[0]._id, {'$set' : {owner_pic : res.picture.data.url}});
+		}
+	}, function(e) {
+    		Meteor._debug("Exception from get facebook posts:", e);
+	}));
+};
+
 if (Meteor.isServer) {
   	fs = Npm.require('fs');
 
@@ -25,78 +97,11 @@ if (Meteor.isServer) {
 		site = FBSiteInfo.find({site: facebookURL}).fetch();
 	}
 
-  	var fbPhotos = new Meteor.Cron({
+	fbUpdate();
+
+	var every_midnight = new Meteor.Cron({
 		events:{
-		    "* * * * *" : function () {
-		    	console.log('Updating from facebook...');
-				FBPhotos.remove({});
-				FBGRAPH.get(facebookURL+"/albums?fields=name,photos.limit(" + facebookPostsLimit + ").fields(images,link,name,created_time)", Meteor.bindEnvironment( function(err, res) {
-					res.data.forEach(function(album) {
-						if (album.name == "Timeline Photos"){
-					 		album.photos.data.forEach(function(photo){
-					 			// console.log(photo)
-					 			FBPhotos.insert(photo);
-					 		});
-					 	}
-					 	if (album.name == "Mobile Uploads"){
-					 		album.photos.data.forEach(function(photo){
-					 			// console.log(photo)
-					 			FBPhotos.insert(photo);
-					 		});
-					 	}
-					 });
-				}, function(e) {
-	        		Meteor._debug("Exception from get facebook logo:", e);
-	    		}));
-
-	    		//console.log('getPageFBLogo'); 
-				//FBGRAPH.setAccessToken(Meteor.user().services.facebook.accessToken);
-				FBGRAPH.get(facebookURL+"?fields=picture.width(130)", Meteor.bindEnvironment( function(err, res) {
-					if (res.picture) {
-						//console.log(res.picture.data);
-						FBSiteInfo.update(site[0]._id, {'$set' : {logo : res.picture.data.url}});
-					}
-				}, function(e) {
-	        		Meteor._debug("Exception from get facebook logo:", e);
-	    		}));
-
-	    		//console.log('getPageFBData'); 
-				FBGRAPH.get(facebookURL+"?fields=about,name,phone", Meteor.bindEnvironment( function(err, res) {
-					//console.log(res);
-					if (res.about) {
-						FBSiteInfo.update(site[0]._id, {'$set' : {about : res.about}});
-					}
-					if (res.name) {
-						FBSiteInfo.update(site[0]._id, {'$set' : {name : res.name}});
-					}
-					if (res.phone) {
-						FBSiteInfo.update(site[0]._id, {'$set' : {phone : res.phone}});
-					}
-				}, function(e) {
-		    		Meteor._debug("Exception from get facebook data:", e);
-				}));
-
-				FBGRAPH.get(facebookURL+"?posts.limit(1)", Meteor.bindEnvironment( function(err, res) {
-					// console.log(res);
-					if (res.posts) {
-						FBSiteInfo.update(site[0]._id, {'$set' : {posts : res.posts.data[0].message}});
-					}
-				}, function(e) {
-			    		Meteor._debug("Exception from get facebook posts:", e);
-				}));
-
-				FBGRAPH.get(facebookURLOwner+"?fields=email,name,about,picture.width(130)", Meteor.bindEnvironment( function(err, res) {
-					// console.log(res);
-					if (res.name){
-						FBSiteInfo.update(site[0]._id, {'$set' : {owner_name : res.name}});
-					}
-					if (res.picture) {
-						FBSiteInfo.update(site[0]._id, {'$set' : {owner_pic : res.picture.data.url}});
-					}
-				}, function(e) {
-			    		Meteor._debug("Exception from get facebook posts:", e);
-				}));
-			}
+		    "* 0 * * *" : fbUpdate
 		}
 	});
 
