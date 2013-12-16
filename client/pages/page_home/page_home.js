8890e3afbc6d4959ab82b04c28b73e2bf80132ce
Anders Åslund <anders.aslund@me.com>
2013-12-16 23:23:24 +0100

    Facebook follow

--- client/pages/page_home/page_home.js
+++ client/pages/page_home/page_home.js
@@ -74,21 +74,6 @@ Template.page_home.thirdthreephotos = function (){
 //         return ((m % n) + n) % n;
 // };
 
-Template.page_home.rendered = function(){
-//add twitter button
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
-
-//add facebook button
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/sv_SE/all.js#xfbml=1";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-};
-
 Template.page_home.about = function(){
 	var site = FBSiteInfo.find({site: facebookURL}).fetch();
 	if (site.length > 0)
