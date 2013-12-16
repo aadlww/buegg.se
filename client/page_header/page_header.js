f4f81db25d5df612a84fc36354c5a66ee6d7392d
Anders Åslund <anders.aslund@me.com>
2013-12-16 22:38:01 +0100

    Reload and navlink fix

--- client/page_header/page_header.js
+++ client/page_header/page_header.js
@@ -1,36 +1,39 @@
-$(document).mouseup(function (e)
-{
-  var container = $("#nav");
-  var container2 = $("#menu-icon");
-  var container3 = $("#menu-icon-img");
+// $(document).mouseup(function (e)
+// {
+//   var container = $("#nav");
+//   var container2 = $("#menu-icon");
+//   var container3 = $("#menu-icon-img");
 
-  if (!container.is(e.target)&& !container2.is(e.target)&& !container3.is(e.target))
-    {
-      $("#nav").slideUp();
-      // $(this).toggleClass("active");
-    }
-});
+//   if (!container.is(e.target)&& !container2.is(e.target)&& !container3.is(e.target))
+//     {
+//       $("#nav").slideUp();
+//       // $(this).toggleClass("active");
+//     }
+// });
 
-Template.page_header.events ={
-  'click #menu-icon': function () {
-    $("#nav").slideToggle();
-    $(this).toggleClass("active");
-  },
-  'click .navBtn': function (e) {
-    if($(e.currentTarget).attr('id')){}
-    $("#nav").slideUp();
-    $('.selected').removeClass('selected');
-    $(e.currentTarget).addClass('selected');
-  } 
+// Template.page_header.events ={
+//   'click #menu-icon': function () {
+//     $("#nav").slideToggle();
+//     $(this).toggleClass("active");
+//   },
+//   'click .navBtn': function (e) {
+//     if($(e.currentTarget).attr('id')){}
+//     $("#nav").slideUp();
+//     $('.selected').removeClass('selected');
+//     $(e.currentTarget).addClass('selected');
+//   } 
+// };
+
+
+// Template.page_header.helpers({
+//   listPages: function(){
+//     return Pages.find();
+//   }
+// });
+
+Template.page_header.listPages = function(){
+  return Pages.find();
 };
-
-
-Template.navlinks.helpers({
-  listPages: function(){
-    return Pages.find();
-  },
-});
-
 
 Template.page_header.fbname = function(){
   var site = FBSiteInfo.find({site: facebookURL}).fetch();
@@ -40,3 +43,6 @@ Template.page_header.fbname = function(){
     return site[0].name;
   }
 };
+
+
+
