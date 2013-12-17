eb45b94710c0ef223f607b9fd015b24f8c2b84cc
Anders Åslund <anders.aslund@me.com>
2013-12-17 18:37:04 +0100

    Fler nyhetsposter!

--- model.js
+++ model.js
@@ -4,7 +4,7 @@ FBSiteInfo = new Meteor.Collection("fbsiteinfo");
 
 facebookURL = "hammarobygg"; //TODO: check if in database and if so set it
 facebookURLOwner = "patric.sundell"; //TODO: check if in database and if so set it
-facebookPostsLimit = 6; //TODO: check if in database and if so set it
+facebookPostsLimit = 32; //TODO: check if in database and if so set it
 loadingMsg = 'Välkommen!'; //TODO: check if in database and if so set it
 loadingIcon = '/images/loader2.gif'; //TODO: check if in database and if so set it
 
