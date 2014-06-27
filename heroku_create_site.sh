d73dccdfe8b444fb75a738df11ec148a89963ab1
Anders Åslund <anders.aslund@me.com>
2014-06-27 05:55:30 +0200

    Added a deployment script for heroky

---
+++ heroku_create_site.sh
@@ -0,0 +1,5 @@
+export APPLICATION_NAME="teknoir" &&
+  heroku create --stack cedar --buildpack https://github.com/tvararu/heroku-buildpack-meteorite.git --region eu $APPLICATION_NAME &&
+  heroku addons:add mongolab:sandbox -a $APPLICATION_NAME &&
+  heroku config:add MONGO_URL=`heroku config:get MONGOLAB_URI -a $APPLICATION_NAME` ROOT_URL=http://$APPLICATION_NAME.herokuapp.com/ -a $APPLICATION_NAME &&
+  heroku labs:enable websockets -a $APPLICATION_NAME
