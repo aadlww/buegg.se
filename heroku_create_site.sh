afc97d205635bd93043c9e4275701c7ec863fcc9
Anders Åslund <anders.aslund@me.com>
2016-04-14 20:10:40 +0200

    Update to meteor 1.3.1

--- heroku_create_site.sh
+++ heroku_create_site.sh
@@ -1,4 +1,4 @@
-export APPLICATION_NAME="hammarobyggokakel" &&
+export APPLICATION_NAME="hammarobygg" &&
   heroku create --stack cedar --buildpack https://github.com/tvararu/heroku-buildpack-meteorite.git --region eu $APPLICATION_NAME &&
   heroku addons:add mongolab:sandbox -a $APPLICATION_NAME &&
   heroku config:add MONGO_URL=`heroku config:get MONGOLAB_URI -a $APPLICATION_NAME` ROOT_URL=http://$APPLICATION_NAME.herokuapp.com/ -a $APPLICATION_NAME &&
