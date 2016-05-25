16fdda41e79dbc8113230863703d6e076023e480
Anders Åslund <anders.aslund@me.com>
2016-05-25 22:27:32 +0200

    AppId

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 app = {
     service: 'hammarobygg',
-    appId: '1412397495650900',
-    secret: '6a6b8d8c564d7afc8e1475f4c431f825'
+    appId: '749583405177364',
+    secret: 'e5294779bc62c6b3b0b3752805888903'
 };
 facebookAccessToken='EAAKpvfyYWhQBABn9zxYUJzjPFD22xZCk5dZCEuNPHLYeGW6exiHB04Go8aZBhzWyTNPk7m0ZAhZASJmErCj52ggX4ZABwMNfaZB5n7ntdTrSNsA7iGFAIfqnkPAru3cxnXTJtw1eZBpdP8dl9xtsvVkKMLIauLGedsLxDYmLCDWvpAZDZD';
